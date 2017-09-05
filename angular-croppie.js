b4402aed7728871ae71adae345dd274e65e7a8ab
Thomas Richner <thomasrichner@oviva.ch>
2017-09-05 14:59:45 +0200

    Added more options.

--- angular-croppie.js
+++ angular-croppie.js
@@ -11,13 +11,22 @@ angular.module('ovi.croppie', []).
     controller: function ($scope, $element) {
       var ctrl = this;
 
-      var c = new Croppie($element[0], {
+      var options = angular.extend(
         viewport: {
           width: 200,
           height: 200
-        },
+        }
+      , $scope.options);
+
+      var resultOptions = angular.extend(
+        {type:'canvas', format:'jpeg'},
+        options.result
+      );
+
+      var c = new Croppie($element[0], {
+        options,
         update: function () {
-          c.result({type:'canvas', format:'jpeg'}).then(function(img) {
+          c.result(resultOptions).then(function(img) {
             $scope.$apply(function () {
               ctrl.ngModel = img;
             });
