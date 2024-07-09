17f01027e00c03590419d2425cba1fa233e790b0
Simon Mary <simon.mary@oviva.com>
2024-07-09 16:20:15 +0200

    CORE-4673: use  to explicitly set annotations, see https://docs.angularjs.org/api/ng/directive/ngApp\#with-ngstrictdi-

--- angular-croppie.js
+++ angular-croppie.js
@@ -8,38 +8,43 @@ angular.module('ovi.croppie', []).
       ngModel: '=',
       options: '<'
     },
-    controller: function ($scope, $element) {
-      var ctrl = this;
-
-      var options = angular.extend({
-        viewport: {
-          width: 200,
-          height: 200
-        }
-      }, ctrl.options);
-
-      var resultOptions = angular.extend(
-        {type:'canvas', format:'jpeg'},
-        options.result
-      );
-
-      options.update = function () {
-        c.result(resultOptions).then(function(img) {
-          $scope.$apply(function () {
-            ctrl.ngModel = img;
-          });
-        });
-      };
-
-      var c = new Croppie($element[0], options);
-
-      $scope.$watch(function(){
-        return ctrl.src;
-      }, function (newSrc) {
-
-        if(!ctrl.src) { return; }
-        // bind an image to croppie
-        c.bind({ url: newSrc, zoom: 0 });
-      });
-    }
+    controller: Controller
   });
+
+
+function Controller($scope, $element) {
+  var ctrl = this;
+  
+  var options = angular.extend({
+    viewport: {
+      width: 200,
+      height: 200
+    }
+  }, ctrl.options);
+  
+  var resultOptions = angular.extend(
+    {type:'canvas', format:'jpeg'},
+    options.result
+  );
+  
+  options.update = function () {
+    c.result(resultOptions).then(function(img) {
+      $scope.$apply(function () {
+        ctrl.ngModel = img;
+      });
+    });
+  };
+  
+  var c = new Croppie($element[0], options);
+  
+  $scope.$watch(function(){
+    return ctrl.src;
+  }, function (newSrc) {
+    
+    if(!ctrl.src) { return; }
+    // bind an image to croppie
+    c.bind({ url: newSrc, zoom: 0 });
+  });
+}
+
+Controller.$inject = ['$scope', '$element'];
